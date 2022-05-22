43f23e1ff651effed927146ef584998b376044ff
pupsik <iliaman1993@gmail.com>
2022-05-22 22:57:30 +0300

    Начальное понимание работы с sqlalchemy имеется

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -29,6 +30,31 @@ class Profiles(db.Model):
 
     def __repr__(self):
         return f"<profiles {self.id}>"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Главная")
+
+
+@app.route("/register", methods=("POST", "GET"))
+def register():
+    if request.method == "POST":
+        #Не збыть сделать проверку коектности данных WTForms помогут
+        try:
+            hash= generate_password_hash(request.form['psw'])
+            u = Users(email=request.form['email'], psw=hash)
+            db.session.add(u)
+            db.session.flush()
+
+            p = Profiles(name=request.form['name'], old=request.form['old'], city=request.form['city'], user_id=u.id)
+            db.session.add(p)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print("Error add into BD")
+
+    return render_template("register.html", title="Регистрация")
 
 
 if __name__ == "__main__":
